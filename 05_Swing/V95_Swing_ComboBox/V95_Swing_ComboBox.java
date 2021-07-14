82d4825ee75c62c2afac290a8429ab0c8515ba4b
Manuel <ManuelCarabiac@gmail.com>
2021-07-14 10:54:43 +0200

    Ejercicio ComboBox

--- 05_Swing/V95_Swing_ComboBox/V95_Swing_ComboBox.java
+++ 05_Swing/V95_Swing_ComboBox/V95_Swing_ComboBox.java
@@ -1,6 +1,14 @@
 package V95_Swing_ComboBox;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class V95_Swing_ComboBox {
@@ -37,8 +45,55 @@ class LaminaComboBox extends JPanel{
 	
 	public LaminaComboBox() {
 		
+		setLayout(new BorderLayout());
+		
+		// Añadimos el texto en la lamina con una etiqueta
+		
+		texto = new JLabel("En un lugar de Vallekas de cuyo barrio...");
+		
+		texto.setFont(new Font("Serif", Font.PLAIN,18));
+		
+		add(texto, BorderLayout.CENTER);
+		
+		//creamos una segunda lamina para añadir al norte del marco
+		
+		JPanel laminaNorte = new  JPanel();
+		
+		micombo= new JComboBox();
+		
+		micombo.setEditable(true);
+		
+		micombo.addItem("Serif");
+		
+		micombo.addItem("SansSerif");
+		
+		micombo.addItem("MonoSpaced");
+		
+		micombo.addItem("Dialog");
+		
+		Evento_combo mievento = new Evento_combo();
+		
+		micombo.addActionListener(mievento);
+		
+		laminaNorte.add(micombo);
+		
+		add(laminaNorte, BorderLayout.NORTH);
 		
 		
 		
 	}
+	
+	private class Evento_combo implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			texto.setFont(new Font((String) micombo.getSelectedItem(), Font.PLAIN,18));
+			
+		}
+		
+	}
+	
+	private JLabel texto;
+	private JComboBox micombo;
 }
