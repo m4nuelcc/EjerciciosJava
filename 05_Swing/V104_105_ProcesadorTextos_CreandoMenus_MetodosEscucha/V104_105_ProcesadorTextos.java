bed738f9e7c81224c81e58c1390bd34075090393
Manuel <ManuelCarabiac@gmail.com>
2021-07-21 12:04:52 +0200

    codigo mejorado y documentado

--- 05_Swing/V104_105_ProcesadorTextos_CreandoMenus_MetodosEscucha/V104_105_ProcesadorTextos.java
+++ 05_Swing/V104_105_ProcesadorTextos_CreandoMenus_MetodosEscucha/V104_105_ProcesadorTextos.java
@@ -61,27 +61,36 @@ class LaminaMenu extends JPanel {
 
 		// ----------------creando submenu fuente------------------------
 
-		configura_menu("Arial", "fuente", "Arial", 9, 10);
+		configura_menu("Arial", "fuente", "Arial", 1, 10);
 
-		configura_menu("Couriel", "fuente", "Couriel", 9, 10);
+		configura_menu("Couriel", "fuente", "Couriel", 1, 10);
 
-		configura_menu("Verdana", "fuente", "Verdana", 9, 10);
+		configura_menu("Verdana", "fuente", "Verdana", 1, 10);
+
+		configura_menu("Agency FB", "fuente", "Agency FB", 1, 10);
 
 		// ----------------creando submenu estilo-------------------------
 
 		configura_menu("Negrita", "estilo", "", Font.BOLD, 1);
 
 		configura_menu("Cursiva", "estilo", "", Font.ITALIC, 1);
+		
+		configura_menu("Romana", "estilo", "", Font.ROMAN_BASELINE, 1);
+		
+		
 
 		// ----------------creando submenu tamaño-------------------------
+		configura_menu("3", "tamagno", "", 1, 3);
 
 		configura_menu("12", "tamagno", "", 1, 12);
 
 		configura_menu("16", "tamagno", "", 1, 16);
-		
+
 		configura_menu("18", "tamagno", "", 1, 18);
-		
+
 		configura_menu("24", "tamagno", "", 1, 24);
+
+		configura_menu("28", "tamagno", "", 1, 28);
 
 		// ------------------------------------------------------------------
 
@@ -99,9 +108,11 @@ class LaminaMenu extends JPanel {
 
 	}
 
-	public void configura_menu(String rotulo, String menu, String tipo_letra, int estilos, int tam) {
+	// metodo para añadir mas opciones al menu
 
-		JMenuItem elem_menu = new JMenuItem(rotulo);
+	public void configura_menu(String textoDelMenu, String menu, String tipo_letra, int estilos, int tam) {
+
+		JMenuItem elem_menu = new JMenuItem(textoDelMenu);
 
 		if (menu == "fuente") {
 
@@ -115,59 +126,59 @@ class LaminaMenu extends JPanel {
 
 			tamagno.add(elem_menu);
 		}
-		
-		elem_menu.addActionListener(new GestionaEventos(rotulo, tipo_letra, estilos, tam));
+
+		elem_menu.addActionListener(new GestionaEventos(textoDelMenu, tipo_letra, estilos, tam));
 
 	}
-	
-	private class GestionaEventos implements ActionListener{
-		
-		String tipo_texto, menu;
-		
-		int estilo_letra, tamagno_letra;
-		
-		GestionaEventos(String elemento, String texto2, int estilo2, int tam_letra){
-			
-			tipo_texto = texto2;
-			
-			estilo_letra= estilo2;
-			
-			tamagno_letra=tam_letra;
-			
-			menu= elemento;
+
+	private class GestionaEventos implements ActionListener {
+
+		String tipo_letra, textoDelMenu;
+
+		int estilos, tam;
+
+		GestionaEventos(String textoDelMenu, String tipo_letra, int estilos, int tam) {
+
+			this.tipo_letra = tipo_letra;
+
+			this.estilos = estilos;
+
+			this.tam = tam;
+
+			this.textoDelMenu = textoDelMenu;
 		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
-		letras=area.getFont();	
-		
-		if(menu=="Arial" || menu=="Couriel" || menu=="Verdana") {
-			
-			estilo_letra= letras.getStyle();
-			
-			tamagno_letra=letras.getSize();
-			
-		}else if (menu=="Cursiva"||menu=="Negrita") {
-			
-			tipo_texto=letras.getFontName();
-			
-			tamagno_letra=letras.getSize();
-			
-		}else if(menu=="12"||menu=="16"||menu=="20"||menu=="24") {
-			
-			estilo_letra=letras.getStyle();
-			
-			tipo_texto=letras.getFontName();
+
+			letras = area.getFont();
+
+			if (textoDelMenu == "Arial" || textoDelMenu == "Couriel" || textoDelMenu == "Verdana"
+					|| textoDelMenu == "Agency FB") {
+
+				estilos = letras.getStyle();
+
+				tam = letras.getSize();
+
+			} else if (textoDelMenu == "Cursiva" || textoDelMenu == "Negrita"|| textoDelMenu=="Romana") {
+
+				tipo_letra = letras.getFontName();
+
+				tam = letras.getSize();
+
+			} else if (textoDelMenu == "12" || textoDelMenu == "16" || textoDelMenu == "20" || textoDelMenu == "24") {
+
+				estilos = letras.getStyle();
+
+				tipo_letra = letras.getFontName();
+			}
+
+			area.setFont(new Font(tipo_letra, estilos, tam));
+
+			System.out.println("tipo " + tipo_letra + " Estilo: " + estilos + " Tamaño:" + tam);
+
 		}
-		
-			
-	    area.setFont(new Font(tipo_texto,estilo_letra,tamagno_letra));
-	    
-	    System.out.println("tipo " + tipo_texto + " Estilo: "+ estilo_letra+ " Tamaño:" + tamagno_letra);
-			
-		}
-		
+
 	}
 
 	JTextPane area;
