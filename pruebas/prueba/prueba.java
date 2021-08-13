bb10c38c618690bf59e220a8db1f3217d4912fd1
Manuel <ManuelCarabiac@gmail.com>
2021-08-13 12:00:34 +0200

    una prueba de ventana

---
+++ pruebas/prueba/prueba.java
@@ -0,0 +1,28 @@
+package prueba;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		JFrame panel = new JFrame();
+		
+		panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel mensaje = new JLabel("hola");
+		
+		panel.setBounds(300,600,300,300);
+		
+		panel.add(mensaje, BorderLayout.NORTH);
+		
+		panel.setVisible(true);
+	
+	}
+
+}
