06e58e6b2e3505094f6cf7c271da4494090bc424
Manuel <ManuelCarabiac@gmail.com>
2021-08-21 11:03:08 +0200

    commit

--- 12_Serializacion/V158_SerialVersionUID/probando.java
+++ 12_Serializacion/V158_SerialVersionUID/probando.java
@@ -3,18 +3,18 @@ package V158_SerialVersionUID;
 public class probando {
 
 	public static void main(String[] args) {
-	
+
 		V158_SerialVersionUID_Receptor_II receptor = new V158_SerialVersionUID_Receptor_II();
-		
+
 		Empleado[] lista;
-		
+
 		lista = receptor.lectura();
-		
-		for(Empleado e: lista) {
-			
-			
-		System.out.println(e);
-			
+
+		for (Empleado e : lista) {
+
+			System.out.println(e);
+
+			System.out.println("nombre: " + e.getNombre());
 		}
 
 	}
