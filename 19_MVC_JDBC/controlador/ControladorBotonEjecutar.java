69dc23184a77c48ba72bfd38238d6a651b3da8e1
Manuel <ManuelCarabiac@gmail.com>
2021-09-30 10:52:30 +0200

    ejercicio terminado

--- 19_MVC_JDBC/controlador/ControladorBotonEjecutar.java
+++ 19_MVC_JDBC/controlador/ControladorBotonEjecutar.java
@@ -2,40 +2,50 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import modelo.EjecutaConsultas;
 import vista.MarcoAplicacion;
 
-public class ControladorBotonEjecutar  implements ActionListener{
+public class ControladorBotonEjecutar implements ActionListener {
 
-	public ControladorBotonEjecutar (MarcoAplicacion marco) {
-		
-		this.marco =marco;
-		
-		
+	public ControladorBotonEjecutar(MarcoAplicacion marco) {
+
+		this.marco = marco;
+
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		
-		
+
+		marco.resultado.setText("");
+
 		String seleccionpais = (String) marco.paises.getSelectedItem();
-		
+
 		String seleccionseccion = (String) marco.secciones.getSelectedItem();
-		
-			
-		marco.resultado.append(consultas.filtraBBDD(seleccionseccion, seleccionpais));
-		
-		
-		marco.resultado.append("\n");
-		
+
+		resultadoConsulta = consultas.filtraBBDD(seleccionseccion, seleccionpais);
+
+		try {
+			while (resultadoConsulta.next()) {
+
+				marco.resultado.append(resultadoConsulta.getString(4) + ",  " + resultadoConsulta.getString(1) + ", "
+						+ resultadoConsulta.getString(2) + ", " + resultadoConsulta.getString(3));
+
+				marco.resultado.append("\n");
+
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 	}
-	
-	
-	
-	
-	private MarcoAplicacion marco; 
-	
+
+	private MarcoAplicacion marco;
+
 	EjecutaConsultas consultas = new EjecutaConsultas();
-	
+
+	private ResultSet resultadoConsulta;
 
 }
--- 19_MVC_JDBC/modelo/EjecutaConsultas.java
+++ 19_MVC_JDBC/modelo/EjecutaConsultas.java
@@ -1,30 +1,78 @@
 package modelo;
 
+import java.sql.*;
+
+
 public class EjecutaConsultas {
-	
-	
-	public String filtraBBDD (String secciones, String paises) {
-		
-		String resultado="";
-		
-		
-		if (!secciones.equals("Todos") && paises.equals("Todos")) {
-			
-			resultado = "has elegido secciones";
-			
-		}else if (!paises.equals("Todos") && secciones.equals("Todos")) {
-			
-			resultado = "has elegido paises";
-			
-		}else {
-			
-			resultado = "has elegido ambos";
-		}
-		
-		
-		
-		return resultado;
-		
+
+	public EjecutaConsultas() {
+
+		miConexion = new Conexion();
+
 	}
+
+	public ResultSet filtraBBDD(String secciones, String pais) {
+
+		Connection conecta = miConexion.daneConexion();
+
+		rs = null;
+
+		try {
+
+			if (!secciones.equals("Todos") && pais.equals("Todos")) {
+
+				enviaConsultaSeccion = conecta.prepareStatement(consultaSeccion);
+
+				enviaConsultaSeccion.setString(1, secciones);
+				
+				rs= enviaConsultaSeccion.executeQuery();
+
+			} else if (!pais.equals("Todos") && secciones.equals("Todos")) {
+				
+				enviaConsultaPais = conecta.prepareStatement(consultaPais);
+
+				enviaConsultaPais.setString(1, pais);
+				
+				rs= enviaConsultaPais.executeQuery();
+
+			} else {
+				
+				enviaConsultaPaisSeccion = conecta.prepareStatement(consultaTodos);
+
+				enviaConsultaPaisSeccion.setString(1, secciones);
+				
+				enviaConsultaPaisSeccion.setString(2, pais);
+								
+				rs= enviaConsultaPaisSeccion.executeQuery();
+
+			}
+			
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		
+		
+		 return rs;
+
+	}
+
+
+
+	private Conexion miConexion;
+
+	private PreparedStatement enviaConsultaSeccion, enviaConsultaPais, enviaConsultaPaisSeccion;
+
+	private ResultSet rs;
+
+	private final String consultaSeccion = "SELECT  NOMBREARTICULO, SECCION, PRECIO,PAISDEORIGEN FROM Productos WHERE SECCION=?";
+	
+	private final String consultaPais = "SELECT  NOMBREARTICULO, SECCION, PRECIO,PAISDEORIGEN FROM Productos WHERE PAISDEORIGEN=?";
+	
+	private final String consultaTodos = "SELECT  NOMBREARTICULO, SECCION, PRECIO,PAISDEORIGEN FROM Productos WHERE SECCION=? AND PAISDEORIGEN=?";
+	
+	
 
 }
