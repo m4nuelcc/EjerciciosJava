f134669ed5e544e04b615fc603e4e17692470e3f
Manuel <ManuelCarabiac@gmail.com>
2021-10-06 20:29:51 +0200

    ejercicio donde utilizamos un filechooser para obtener la configuracion de la BBDD desde un fichero txt

--- 22_METADATOS/V227_Ejercicio_Practico_Metadatos_III/AplicacionUniversal_lee_De_Un_Fichero.java
+++ 22_METADATOS/V227_Ejercicio_Practico_Metadatos_III/AplicacionUniversal_lee_De_Un_Fichero.java
@@ -1,8 +1,9 @@
-package V225_226_Ejercicio_Practico_Metadatos_II;
+package V227_Ejercicio_Practico_Metadatos_III;
 
 import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -35,6 +36,8 @@ class MarcoBBDD extends JFrame {
 
 		LaminaBBDD milamina = new LaminaBBDD();
 
+		setTitle("V227_Ejercicio_Practico_Metadatos_III");
+
 		add(milamina);
 
 	}
@@ -54,6 +57,8 @@ class LaminaBBDD extends JPanel {
 		add(areaInformacion, BorderLayout.CENTER);
 
 		add(comboTablas, BorderLayout.NORTH);
+
+		// choose();
 
 		conexionBBDD();
 
@@ -133,16 +138,13 @@ class LaminaBBDD extends JPanel {
 
 		try {
 
-			lecturaDDBB = new FileReader("222_METADATOS/V227_Ejercicio_Practico_Metadatos_III/DatosBBDD.txt");
-			
-			
-			//si no encuentra el fichero abrimos un filechoose, elejimos un fichero
-			// y asi obtenemos la ruta absouluta y lo leemos con filereader()
-		} catch (IOException e) {
+			lecturaDDBB = new FileReader("22_METADATOS/V227_Ejercicio_Practico_Metadatos_III/DatosBBDD.txt");
+
+		} catch (Exception e) {
 
 			JFileChooser chooser = new JFileChooser();
 
-			FileNameExtensionFilter filter = new FileNameExtensionFilter("Documentos txt", "txt");
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("Documento txt", "txt", "txt");
 
 			chooser.setFileFilter(filter);
 
@@ -150,34 +152,38 @@ class LaminaBBDD extends JPanel {
 
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 
+				chooser.getSelectedFile().getAbsolutePath();
+
 				try {
 					lecturaDDBB = new FileReader(chooser.getSelectedFile().getAbsolutePath());
+
 				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
+
 					e1.printStackTrace();
 				}
+
 			}
 
 		}
 
-		try {
-			bufferBBDD = new BufferedReader(lecturaDDBB);
+		bufferBBDD = new BufferedReader(lecturaDDBB);
 
-			for (int i = 0; i < datosconexion.length; i++) {
+		for (int i = 0; i < datosconexion.length; i++) {
 
+			try {
 				datosconexion[i] = bufferBBDD.readLine();
+
+				miConexion = DriverManager.getConnection(datosconexion[0], datosconexion[1], datosconexion[2]);
+
+			} catch (IOException | SQLException e) {
+				
+				e.printStackTrace();
 			}
-
-			miConexion = DriverManager.getConnection(datosconexion[0], datosconexion[1], datosconexion[2]);
-
-			lecturaDDBB.close();
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
 		}
 
 	}
+	
+	
 
 	public void comboTablas() {
 
@@ -200,6 +206,8 @@ class LaminaBBDD extends JPanel {
 		}
 
 	}
+
+	private String ruta;
 
 	private Connection miConexion;
 
